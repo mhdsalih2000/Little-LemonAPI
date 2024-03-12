7ed9d4e85280eec9d34d2ff6fb1aa85914a4fcd0
muhammed salih <142479202+mhdsalih2000@users.noreply.github.com>
2024-03-12 12:37:55 +0530

    Update settings.py

--- little_lemonapi/settings.py
+++ little_lemonapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5t86ar$vc@1mni2&*51qfkv^u2nd!#4erxv!9b4lvo#z(t(*qo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['restoapi.shop' , 'www.restoapi.shop']
 
